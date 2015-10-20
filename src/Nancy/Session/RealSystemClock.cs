5607fefee89e7c303b09fdc18523295f53a4b634
David Lievrouw <david.lievrouw@gmail.com>
2015-10-20 22:01:51 +0200

    Create cache where the sessions will reside in

---
+++ src/Nancy/Session/RealSystemClock.cs
@@ -0,0 +1,21 @@
+﻿namespace Nancy.Session
+{
+    using System;
+
+    /// <summary>
+    /// The default implementation of the SystemClock interface.
+    /// </summary>
+    public class RealSystemClock : ISystemClock
+    {
+        /// <summary>
+        /// Gets the current UTC date and time.
+        /// </summary>
+        public DateTime NowUtc
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+    }
+}
