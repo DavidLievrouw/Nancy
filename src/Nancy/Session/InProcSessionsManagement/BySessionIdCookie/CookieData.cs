77980d91af89f1ebe140a2085cebcc3e08041e23
David Lievrouw <david.lievrouw@gmail.com>
2015-10-24 18:37:42 +0200

    Finish the implementation of the BySessionCookieIdentificationMethod class

--- src/Nancy/Session/InProcSessionsManagement/BySessionIdCookie/CookieData.cs
+++ src/Nancy/Session/InProcSessionsManagement/BySessionIdCookie/CookieData.cs
@@ -1,8 +1,17 @@
 namespace Nancy.Session.InProcSessionsManagement.BySessionIdCookie
 {
+    using System;
+
     internal class CookieData
     {
         public string SessionId { get; set; }
-        public string Hmac { get; set; }
+        public byte[] Hmac { get; set; }
+
+        public override string ToString()
+        {
+            var base64hmac = string.Empty;
+            if (this.Hmac != null) base64hmac = Convert.ToBase64String(this.Hmac);
+            return string.Format("{0}{1}", this.SessionId, base64hmac);
+        }
     }
 }
